3a17b75d38540f7c3c3674a47a387b46cb7b4e34
bhigoreduardo <bhigoreduardo@gmail.com>
2022-10-16 11:24:00 -0300

    Add Exercicio Empresa

--- PostgresSQL/Fisico/1-Empresa/3-Select_Database-Empresa.sql
+++ PostgresSQL/Fisico/1-Empresa/3-Select_Database-Empresa.sql
@@ -1,80 +1,85 @@
-/* SELECT table_name */
-SELECT table_name
-	FROM information_schema.tables
-	WHERE table_schema = 'public'
-	ORDER BY table_name;
+/* Listar nome e Sobrenome de Funcionários ordenado por sobrenome. */
+SELECT nome, sobrenome
+FROM funcionario
+ORDER BY sobrenome ASC;
 
+/* Listar todos os campos de Funcionários ordenados por cidade. */
 SELECT *
-	FROM fruta_a;
+FROM funcionario
+ORDER BY cidade ASC;
 
-/* INNER JOIN */
+/* Liste os funcionários que têm salário superior a R$ 1.000,00 ordenados pelo nome completo. */
 SELECT *
-	FROM fruta_a
-	INNER JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome;
-	
-SELECT *
-	FROM fruta_a, fruta_b
-	WHERE fruta_a.nome = fruta_b.nome;
-	
-SELECT *
-	FROM fruta_a
-	INNER JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome
-	AND fruta_a.nome LIKE 'G%';
-	
-/* LEFT JOIN e LEFT OUTER JOIN */
-SELECT *
-	FROM fruta_a
-	LEFT JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome;
-	
-SELECT *
-	FROM fruta_a
-	LEFT OUTER JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome;
+FROM funcionario
+WHERE salario > 1000
+ORDER BY nome, sobrenome ASC;
 
-SELECT *
-	FROM fruta_a
-	LEFT JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome
-	WHERE fruta_b.nome IS NULL;
-	
-/* RIGHT JOIN e RIGHT OUTER JOIN */
-SELECT *
-	FROM fruta_a
-	RIGHT JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome;
-	
-SELECT *
-	FROM fruta_a
-	RIGHT OUTER JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome;
+/* Liste a data de nascimento e o primeiro nome dos funcionários ordenados do mais novo para o mais velho. */
 
-SELECT *
-	FROM fruta_a
-	RIGHT JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome
-	WHERE fruta_a.nome IS NULL;
-	
-/* FULL JOIN */
-SELECT *
-	FROM fruta_a
-	FULL JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome;
+/* Liste os funcionários como uma lista telefônica (Nome, SobreNome, Fone, Endereço, Cidade, UF). */
+SELECT nome, sobrenome, fone, endereco, cidade, uf
+FROM funcionario;
 
-SELECT *
-	FROM fruta_a
-	FULL OUTER JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome;
-	
-SELECT *
-	FROM fruta_a
-	FULL JOIN fruta_b
-	ON fruta_a.nome = fruta_b.nome
-	WHERE fruta_a.nome IS NULL OR fruta_b.nome IS NULL;
-	
-/* NATURAL JOIN */
-SELECT *
-	FROM fruta_a
-	NATURAL JOIN fruta_b;
+/* Liste o total da folha de pagamento. */
+SELECT SUM(salario) AS folha_pagamento
+FROM funcionario;
+
+/* Liste o nome, Sobrenome, Nome do Departamento e a função de todos os funcionários. */
+SELECT f.nome, f.sobrenome, f.funcao, d.nome
+FROM funcionario f
+INNER JOIN departamento d
+ON f.codigo_departamento = d.codigo;
+
+/* Liste todos os departamentos com seus respectivos gerentes. */
+SELECT d.*, f.nome
+FROM departamento d
+INNER JOIN funcionario f
+ON d.codigo_funcionario = f.codigo;
+
+/* Liste o valor da folha de pagamento de cada departamento, ordenado pelo nome do departamento. */
+SELECT d.nome, SUM(f.salario) AS folha_pagamento
+FROM departamento d
+INNER JOIN funcionario f
+ON d.codigo = f.codigo_departamento
+GROUP BY d.nome;
+
+/* Liste os departamentos dos funcionários que têm a função de Supervisor. */
+SELECT d.nome
+FROM departamento d
+LEFT JOIN funcionario f
+ON d.codigo = f.codigo_departamento
+WHERE f.funcao = 'CEO';
+
+/*  Liste a quantidade de funcionários desta empresa. */
+SELECT COUNT(*) AS funcionarios
+FROM funcionario;
+
+/* Liste o salário médio pago pela empresa. */
+SELECT AVG(salario) AS salario_medio
+FROM funcionario;
+
+/* Liste o menor salário pago pela empresa em cada departamento. */
+SELECT d.nome, MIN(salario) AS salario_minino
+FROM departamento d
+LEFT JOIN funcionario f
+ON d.codigo = f.codigo_departamento
+GROUP BY d.nome;
+
+/* Liste o nome completo de todos os funcionários que nasceram antes de 01/01/1980. */
+
+
+/* Liste o nome do departamento e do funcionário ordenados por departamento e funcionário. */
+SELECT d.nome AS departamento, f.nome AS funcionario
+FROM funcionario f
+LEFT JOIN departamento d
+ON f.codigo_departamento = d.codigo
+ORDER BY d.nome, f.nome;
+
+/* Liste os nomes dos funcionários que moram em Itumbiara e que exerçam a função de telefonista. */
+SELECT nome
+FROM funcionario
+WHERE cidade = 'Itumbiara' AND funcao = 'Telefonista';
+
+/* Liste os nomes dos funcionários que trabalham no Departamento Pessoal. */
+
+/* Liste o nome e o departamento de todos os funcionários que ganham mais do que algum gerente. */
---
+++ PostgresSQL/Fisico/1-Empresa/README.md
@@ -0,0 +1,44 @@
+## Empresa
+
+De acordo com a estrutura do banco de dados abaixo elabore as consultas: 
+Seguem as descrições das tabelas:
+- Funcionarios (Codigo, Nome, SobreNome, DataNasci, CPF, RG, Endereço, CEP, Cidade, UF, Fone, CodigoDepartamento, Funcao, Salario)
+- Departamentos ( Codigo, Nome, Localizacao, CodigoFuncionarioGerente)
+
+Implementar o as tabelas em um banco no PostgreSQL, preencher com no mínimo 10 registros para a tabela Funcionarios e 04 para tabela Departamentos.
+
+01 - Listar nome e Sobrenome de Funcionários ordenado por sobrenome.
+
+02 - Listar todos os campos de Funcionários ordenados por cidade.
+
+03 - Liste os funcionários que têm salário superior a R$ 1.000,00 ordenados pelo nome completo.
+
+04 - Liste a data de nascimento e o primeiro nome dos funcionários ordenados do mais novo para o mais velho.
+
+05 - Liste os funcionários como uma lista telefônica (Nome, SobreNome, Fone, Endereço, Cidade, UF).
+
+06 - Liste o total da folha de pagamento.
+
+07 - Liste o nome, Sobrenome, Nome do Departamento e a função de todos os funcionários.
+
+08 - Liste todos os departamentos com seus respectivos gerentes.
+
+09 - Liste o valor da folha de pagamento de cada departamento, ordenado pelo nome do departamento.
+
+10 - Liste os departamentos dos funcionários que têm a função de Supervisor.
+
+11 - Liste a quantidade de funcionários desta empresa.
+
+12 - Liste o salário médio pago pela empresa.
+
+13 - Liste o menor salário pago pela empresa em cada departamento.
+
+14 - Liste o nome completo de todos os funcionários que nasceram antes de 01/01/1980.
+
+15 - Liste o nome do departamento e do funcionário ordenados por departamento e funcionário.
+
+16 - Liste os nomes dos funcionários que moram em Itumbiara e que exerçam a função de telefonista.
+
+17 - Liste os nomes dos funcionários que trabalham no Departamento Pessoal.
+
+18 - Liste o nome e o departamento de todos os funcionários que ganham mais do que algum gerente.
