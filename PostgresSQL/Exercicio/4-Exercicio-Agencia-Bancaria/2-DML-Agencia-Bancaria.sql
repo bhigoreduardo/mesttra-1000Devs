b8e55a2cd9502dafd4db9377da75e53c458ed118
unknown <bhigoreduardo@gmail.com>
2022-10-21 18:42:38 -0300

    DML funcionario and agencia

---
+++ PostgresSQL/Exercicio/4-Exercicio-Agencia-Bancaria/2-DML-Agencia-Bancaria.sql
@@ -0,0 +1,13 @@
+/* Insert funcionario */
+INSERT INTO funcionario
+	(nome)
+	VALUES
+	('Blast'),
+	('Borus');
+
+/* Insert agencia */
+INSERT INTO agencia
+	(nome, endereco, telefone, gerente)
+	VALUES
+	('Hero Association', 'Cidade A', '00111111111', 1),
+	('Monster Association', 'Cidade Z', '00999999999', 2);
