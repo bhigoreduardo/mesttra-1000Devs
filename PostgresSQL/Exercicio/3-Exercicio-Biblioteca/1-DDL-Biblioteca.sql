5c5213f4084d5b45778abfcbf865afdd484171f6
unknown <bhigoreduardo@gmail.com>
2022-10-22 13:42:37 -0300

    Created entities

--- PostgresSQL/Exercicio/3-Exercicio-Biblioteca/1-DDL-Biblioteca.sql
+++ PostgresSQL/Exercicio/3-Exercicio-Biblioteca/1-DDL-Biblioteca.sql
@@ -9,3 +9,40 @@ CREATE DATABASE biblioteca
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
+/* Created Table aluno */
+CREATE TABLE IF NOT EXISTS aluno (
+	matricula SERIAL PRIMARY KEY,
+	nome VARCHAR(40) NOT NULL,
+	sobrenome VARCHAR(60) NOT NULL,
+	endereco VARCHAR(100),
+	genero CHAR(1),
+	data_nasc DATE,
+	quant_livro INTEGER
+);
+
+/* Created Table autor */
+CREATE TABLE IF NOT EXISTS autor (
+	cod_autor SERIAL PRIMARY KEY,
+	nome VARCHAR(40) NOT NULL,
+	sobrenome VARCHAR(60) NOT NULL,
+	genero CHAR(1),
+	data_nasc DATE,
+	cpf VARCHAR(14) NOT NULL UNIQUE
+);
+
+/* Created Table livro */
+CREATE TABLE IF NOT EXISTS livro (
+	cod_livro SERIAL PRIMARY KEY,
+	titulo VARCHAR(40) NOT NULL,
+	cod_autor INTEGER NOT NULL REFERENCES autor,
+	status_emp BOOLEAN DEFAULT false
+);
+
+/* Created Table emprestimo */
+CREATE TABLE IF NOT EXISTS emprestimo (
+	cod_emprestimo SERIAL PRIMARY KEY,
+	data DATE NOT NULL DEFAULT (CURRENT_DATE),
+	cod_livro INTEGER NOT NULL REFERENCES livro,
+	matricula INTEGER NOT NULL REFERENCES aluno,
+	devolucao BOOLEAN DEFAULT false
+);
