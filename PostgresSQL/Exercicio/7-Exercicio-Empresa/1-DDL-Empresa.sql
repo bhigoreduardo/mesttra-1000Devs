fb09ca242fa6d771439a53eeac2c1ad8f73dedba
unknown <bhigoreduardo@gmail.com>
2022-10-22 12:46:15 -0300

    Created Tables empresa

--- PostgresSQL/Exercicio/7-Exercicio-Empresa/1-DDL-Empresa.sql
+++ PostgresSQL/Exercicio/7-Exercicio-Empresa/1-DDL-Empresa.sql
@@ -8,3 +8,34 @@ CREATE DATABASE empresa
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+/* Created Table funcionario */
+CREATE TABLE IF NOT EXISTS funcionario (
+	codigo INTEGER PRIMARY KEY,
+	nome VARCHAR(100),
+	sobrenome VARCHAR(100),
+	datanasci DATE,
+	cpf VARCHAR(14),
+	rg VARCHAR(10),
+	endereco VARCHAR(140),
+	cep VARCHAR(8),
+	cidade VARCHAR(50),
+	uf VARCHAR(2),
+	fone VARCHAR(20),
+	funcao VARCHAR(40),
+	salario NUMERIC(9,2),
+	codigodepartamento INTEGER
+);
+
+/* Created Table departamento */
+CREATE TABLE IF NOT EXISTS departamento (
+	codigo INTEGER PRIMARY KEY,
+	nome VARCHAR(60),
+	localizacao VARCHAR(60),
+	cod_func_gerente INTEGER REFERENCES funcionario(codigo)
+);
+
+/* Added Foreign Key funcionario */
+ALTER TABLE funcionario
+	ADD CONSTRAINT fk_funcionario_departamento
+	FOREIGN KEY (codigodepartamento) REFERENCES departamento;
